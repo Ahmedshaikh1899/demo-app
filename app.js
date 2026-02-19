46edea23599c243d4a81abed374524765f2ed769
Ahmedshaikh1899 <ahmedshaikh1899@gmail.com>
2026-02-19 07:12:53 +0000

    updated to k8 version

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    res.end("Hello from SECOND APP it is updated version ;-), This is from k8s");
+    res.end("Hello from SECOND APP it is updated version ;-), This is from k8s updateddddddddddddd");
 });
 
 server.listen(4000, () => {
