9761d7d56ae94791f402993adaa1762e03863a08
Ahmedshaikh1899 <ahmedshaikh1899@gmail.com>
2026-02-21 06:43:38 +0000

    2102 Test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    res.end("Hello from SECOND APP it is updated version ;-), This is from k8s updateddddddddddddd and it is pushed automatically buddyyy niceeeey");
+    res.end("Hello from SECOND APP it is updated version ;-), This is from k8s updateddddddddddddd and it is pushed automatically buddyyy niceeeey  v22");
 });
 
 server.listen(4000, () => {
